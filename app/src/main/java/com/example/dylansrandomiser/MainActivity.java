c23ead99069a37c60618ff81725119a9840a2042
Dylan Rafferty <draff1800@gmail.com>
2019-07-29 04:10:55 +0100

    Added random number generator functionality (Text, scale, and button)

--- app/src/main/java/com/example/dylansrandomiser/MainActivity.java
+++ app/src/main/java/com/example/dylansrandomiser/MainActivity.java
@@ -3,12 +3,36 @@ package com.example.dylansrandomiser;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.util.*;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button rollButton;
+    TextView resultsTextView;
+    SeekBar selectionSeekBar;
+
+    private View.OnClickListener rollButtonClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            int randomInt = new Random().nextInt(selectionSeekBar.getProgress()+1);
+            resultsTextView.setText(Integer.toString(randomInt));
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rollButton = findViewById(R.id.rollButton);
+        resultsTextView = findViewById(R.id.resultsTextView);
+        selectionSeekBar = findViewById(R.id.selectionSeekBar);
+
+        rollButton.setOnClickListener(rollButtonClickListener);
     }
 }
